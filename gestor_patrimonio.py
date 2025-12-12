8c9a7d84e928366bb32e1d20e39aafb8bd7cc374
George Dantas <estudos3396@gmail.com>
2025-12-12 14:24:11 -0300

    Implementa listas dinâmicas, loop while e relatórios detalhados

--- gestor_patrimonio.py
+++ gestor_patrimonio.py
@@ -1,30 +1,41 @@
 salario = float(input("Digite seu salário: "))
 
 ###Gastos fixos
-gastos = [170, 985, 385, 250]
-gastos_soma = sum(gastos)
-lista_gastos = ['Casa', 'Moto', 'Suhai', 'Reserva']
+gastos_fixos = [170, 985, 385, 250]
+valor_fixo = sum(gastos_fixos)
+nomes_fixos = ['Casa', 'Moto', 'Suhai', 'Reserva']
 
 ###Gastos variaveis
-perguntas = ['Cartão', 'Lazer', 'Mercado', 'Gasolina', 'Roupa']  
+gastos_variaveis = []
+nomes_variaveis = []
 
 #Adiciona itens as listas
-for item in perguntas:
-    valor = float(input(f"Quanto você gastou com {item}? "))
+continuar = input('Deseja adicionar gasto? [S/N]: ').upper()
+
+while continuar == 'S':
+    item = input('Qual gasto a ser adicionado? (ex. Cartão, delivery, uber, etc.): ')
+    valor = float(input(f"Quanto você gastou com {item}?: "))
     
-    gastos.append(valor)
-    lista_gastos.append(item)
+    gastos_variaveis.append(valor)
+    nomes_variaveis.append(item)
+
+    continuar = input('Adicionar outro gasto? [S/N]: ').upper()
+    valor_variavel = sum(gastos_variaveis)
+
 ###Objetivo reserva de emergencia
-reserva_emergenciaMeta = 10000 / gastos[3]
+reserva_emergenciaMeta = 10000 / gastos_fixos[3]
 
 ###Total de gastos e valor que sobra do salario
-total_gastos = sum(gastos)
+total_gastos = sum(gastos_fixos + gastos_variaveis)
 saldo_final = salario - total_gastos
 
-print (f'O valor total das despesas é: {total_gastos}, sendo que {gastos_soma} foram gastos fixos.')
+print (f'O valor total das despesas é: {total_gastos}')
 print (f'O valor que sobra depois de pagar as contas é: {saldo_final}')
 
-print (f'Para atingir a meta da reserva de emergencia, levará {reserva_emergenciaMeta} meses')
+print(f"Relatório: R$ {valor_fixo:.2f} Fixos + R$ {valor_variavel:.2f} Variáveis")
+print(f"Total Geral: R$ {total_gastos:.2f}\n")
+
+print (f'Para atingir a meta da reserva de emergencia, levará {reserva_emergenciaMeta} meses!\n')
 
 #logica de decisão
 if saldo_final < 0:
@@ -32,5 +43,5 @@ if saldo_final < 0:
 elif saldo_final < salario*0.10:
     print ("!Cuidado! \nAumente a renda para que tenha sobra do salario")
 else:
-    print ("Parabéns!!\n A saúde financeira está indo bem!")
+    print ("Parabéns!!\nA saúde financeira está indo bem!")
 
